b4a825092ae2b4cae8b50932a6b81c9764abad21
mina0011 <minawagdy429@gmail.com>
2022-11-28 22:15:23 +0200

    first Commit

--- src/test/java/org/example/pages/P01_register.java
+++ src/test/java/org/example/pages/P01_register.java
@@ -10,4 +10,50 @@ public class P01_register {
         return driver.findElement(By.className("ico-register"));
     }
 
+    public WebElement gender()
+    {
+        return driver.findElement(By.id("gender-male"));
+    }
+
+    public WebElement FirstName()
+    {
+        return driver.findElement(By.id("FirstName"));
+    }
+    public WebElement LastName()
+    {
+        return driver.findElement(By.id("LastName"));
+    }
+    public WebElement DayList()
+    {
+        return driver.findElement(By.name("DateOfBirthDay"));
+    }
+    public WebElement MonthList()
+    {
+        return driver.findElement(By.name("DateOfBirthMonth"));
+    }
+    public WebElement YearList()
+    {
+        return driver.findElement(By.name("DateOfBirthYear"));
+    }
+    public WebElement Email()
+    {
+        return driver.findElement(By.id("Email"));
+    }
+    public WebElement Password()
+    {
+        return driver.findElement(By.id("Password"));
+    }
+    public WebElement ConfirmPassword()
+    {
+        return driver.findElement(By.id("ConfirmPassword"));
+    }
+    public WebElement RegisterButton()
+    {
+        return driver.findElement(By.id("register-button"));
+    }
+    public WebElement SuccessMessage()
+    {
+        return driver.findElement(By.className("result"));
+    }
+
 }
--- src/test/java/org/example/stepDefs/D01_Register.java
+++ src/test/java/org/example/stepDefs/D01_Register.java
@@ -1,7 +1,13 @@
 package org.example.stepDefs;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.example.pages.P01_register;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.asserts.SoftAssert;
 
 public class D01_Register {
 
@@ -13,5 +19,60 @@ public class D01_Register {
         register.registerLink().click();
     }
 
+    @When("user select gender type")
+    public void userSelectGenderType()
+    {
+        register.gender().click();
+    }
 
+    @And("user enter first name {string} and last name {string}")
+    public void userEnterFirstNameAndLastName(String FirstName, String LastName)
+    {
+        register.FirstName().sendKeys(FirstName);
+        register.LastName().sendKeys(LastName);
+    }
+
+    @And("user enter date of birth")
+    public void userEnterDateOfBirth()
+    {
+        Select dayList = new Select(register.DayList());
+        dayList.selectByValue("30");
+
+        Select MonthList = new Select(register.MonthList());
+        MonthList.selectByValue("11");
+
+        Select YearList = new Select(register.YearList());
+        YearList.selectByValue("2001");
+    }
+
+    @And("user enter email {string} field")
+    public void userEnterEmailField(String Email)
+    {
+        register.Email().sendKeys(Email);
+    }
+
+    @And("user fills Password fields {string} {string}")
+    public void userFillsPasswordFields(String Password, String ConfirmPassword)
+    {
+        register.Password().sendKeys(Password);
+        register.ConfirmPassword().sendKeys(ConfirmPassword);
+    }
+
+    @And("user clicks on register button")
+    public void userClicksOnRegisterButton()
+    {
+        register.RegisterButton().click();
+    }
+
+    @Then("success message is displayed")
+    public void successMessageIsDisplayed()
+    {
+        SoftAssert soft = new SoftAssert();
+        boolean SuccessfulMessage = register.SuccessMessage().isDisplayed();
+        soft.assertTrue(SuccessfulMessage);
+        String MessageColor = register.SuccessMessage().getCssValue("color");
+        String ActualMessageColor = Color.fromString(MessageColor).asHex();
+        soft.assertEquals(ActualMessageColor,"#4cb17c");
+        soft.assertAll();
+    }
 }
